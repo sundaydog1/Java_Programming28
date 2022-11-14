b4b5641b44c923db4301ecca0b8a77fd4f4a9a3f
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-11-14 10:38:01 -0500

    Java Programming - Day22

---
+++ src/day22_arrayList/Test.java
@@ -0,0 +1,25 @@
+package day22_arrayList;
+
+import utilities.ArraysUtility;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30, 40, 50, 60, 70, 90};
+
+        boolean has25 = ArraysUtility.contains(numbers, 25);
+
+        System.out.println("has25 = " + has25);
+
+        System.out.println("--------------------------------------------");
+
+        String[] students = {"Sumeye", "Hamza", "Marika"};
+
+        System.out.println( ArraysUtility.contains(students, "Khashayar")  );
+
+
+
+    }
+
+}
